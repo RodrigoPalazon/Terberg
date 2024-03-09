be1160cd059ebefe4f7c6642114095e32b84b9b8
Rodrigo Palazon <palazonbatera@gmail.com>
2024-03-09 19:42:45 +0100

    Create DB sqlite# connection. Create 'Website' and 'Vehicles'tables with the related data.

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,43 +1,34 @@
 import sqlite3
-import sys
-import os
-
-# Calculate the absolute path to the directory containing 'cleaned_data.py'
-data_dir = os.path.abspath("C:/Users/RodrigoRiquenaPalazo/Documents/Rockfeather/Traineeship/Terberg_correct_repo/data/resources")
-
-# Add this directory to sys.path
-sys.path.append(data_dir)
-
-# Now you can import the 'data' variable from 'cleaned_data.py'
-from vehicles_data import data
-
-# The rest of your script where you use 'data'
+from vehicles_data import vehicles_data
+from website_data import website_data
 
 # Create a connection to the database
-conn = sqlite3.connect('../data/db/terberg.db')
+conn = sqlite3.connect('terberg.db')
 
 # Create a cursor object
 c = conn.cursor()
 
-# Create table
+# Create table vehicles
 c.execute("""CREATE TABLE IF NOT EXISTS listings
              (  name TEXT, 
                 subcategory TEXT, 
                 price REAL)
           """)
 
-# Insert data into table
-for item in data:
+# Create table websites
+c.execute("""CREATE TABLE IF NOT EXISTS website
+             (  name TEXT, 
+                url TEXT, 
+                listing INTEGER)
+          """)
+
+# Insert data into vehicles table
+for item in vehicles_data:
     c.execute("INSERT INTO listings VALUES (?, ?, ?)", (item['name'], item['subcategory'], item['price']))
 
-# # Query the Database
-# c.execute("SELECT rowid,* FROM listings")
-# items =c.fetchall()   
-# # print(items)
-
-# for item in items:
-#     print(str(item[0]) + " " + item[1] + "| \t" + item[2] + " " + str(item[3]))
-# # print(c.fetchone()[0])
+# Insert data into website table
+for item in website_data:
+    c.execute("INSERT INTO website VALUES (?, ?, ?)", (item['name'], item['url'], item['listing']))
 
 # # Commit changes and close connection
 conn.commit()
